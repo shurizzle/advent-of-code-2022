282aa2c3fd43eebd273aa06fe607499e5324853e
shurizzle <shura1991@gmail.com>
2022-12-05 08:21:28 +0100

    Convert day 3 to scryer

--- prolog/3.pl
+++ prolog/3.pl
@@ -1,66 +1,71 @@
-:- [prelude].
+:- module('3', [parse//1, part1/2, part2/2]).
 
-input_file("inputs/3.txt").
+:- use_module(library(charsio)).
+:- use_module(library(between)).
+:- use_module(library(lists)).
+:- use_module(library(lambda)).
 
-% tests {{{
-tinput("vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw").
+blank --> [C], { char_type(C, whitespace) }.
+blanks --> blank, !, blanks.
+blanks --> [].
 
-test1(Data) :- part1(Data, Res), print1(Res), nl.
-test1_from_string(Input) :- input_from_string(Input, Data), test1(Data).
-test1 :- tinput(Data), test1_from_string(Data).
+nl --> "\n".
 
-test2(Data) :- part2(Data, Res), print2(Res), nl.
-test2_from_string(Input) :- input_from_string(Input, Data), test2(Data).
-test2 :- tinput(Data), test2_from_string(Data).
+character(C) -->
+  [C0],
+  { char_code(C0, C1),
+    between(0'a, 0'z, C1),
+    C is C1 - 0'a + 1 }, !.
+character(C) -->
+  [C0],
+  { char_code(C0, C1),
+    between(0'A, 0'Z, C1),
+    C is C1 - 0'A + 27 }, !.
 
-test(Data) :- test1(Data), test2(Data).
-test_from_string(Input) :- input_from_string(Input, Data), test(Data).
-test :- tinput(Input), test_from_string(Input).
-% }}}
+parse([L|Ls]) --> blanks, line(L), parse_(Ls), blanks.
+parse_([L|Ls]) --> nl, line(L), !, parse_(Ls).
+parse_([]) --> [].
 
-print1(Res) :- print(Res).
-print2(Res) :- print(Res).
-
-parse(Lines) --> blanks, list_of(L, raw_line(line(L)), Lines), blanks.
-line(Is) --> list_of(I, character(I), Is), { length(Is, L), L > 0 }.
-
-character(C) --> [C0], { between(0'a, 0'z, C0), C is C0 - 0'a + 1 }, !.
-character(C) --> [C0], { between(0'A, 0'Z, C0), C is C0 - 0'A + 27 }, !.
+line([I|Is]) --> character(I), !, line_(Is).
+line_([I|Is]) --> character(I), !, line_(Is).
+line_([]) --> [].
 
 common_element(Xs, I) :-
   length(Xs, L0),
-  L is L0/2,
+  L is L0 div 2,
   length(A, L),
   length(B, L),
   append(A, B, Xs),
   member(I, A),
   member(I, B), !.
 
-groups_of_three([A,B,C|_], [A,B,C]).
-groups_of_three([_,_,_|T], G) :-
-  length(T, L), L > 0,
-  groups_of_three(T, G).
+chunks(Xs, Len, Res) :-
+  length(Xs, L),
+  L > 0,
+  (   L > Len
+  ->  length(Res, Len),
+      once(append(Res, _, Xs))
+  ;   Res = Xs
+  ).
+chunks(Xs, Len, Res) :-
+  length(Xs, L),
+  L > Len,
+  length(T0, Len),
+  once(append(T0, Tail, Xs)),
+  chunks(Tail, Len, Res).
 
 part1(Data, Res) :-
-  foldl([Xs, Acc0, Acc]>>(
+  foldl(\Xs^Acc0^Acc^(
     common_element(Xs, I),
     Acc is Acc0 + I
   ), Data, 0, Res).
 part2(Data, Res) :-
-  aggregate_all(
-    sum(X),
-    (
-      groups_of_three(Data, [A,B,C]),
+  findall(X,
+    ( chunks(Data, 3, [A,B,C]),
       once((
         member(X, A),
         member(X, B),
         member(X, C)
-      ))
-    ),
-    Res
-  ).
+      )) ),
+    Xs),
+    sum_list(Xs, Res).
