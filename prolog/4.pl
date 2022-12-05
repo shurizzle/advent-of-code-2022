4276e04aad75eb1dcec4e12585ced18273ce9513
shurizzle <shura1991@gmail.com>
2022-12-05 08:45:51 +0100

    Add day 4

---
+++ prolog/4.pl
@@ -0,0 +1,53 @@
+:- module('4', [parse//1, part1/2, part2/2]).
+
+:- use_module(library(charsio)).
+:- use_module(library(lists)).
+
+:- op(900, xfx, ..).
+
+blank --> [C], { char_type(C, whitespace) }.
+blanks --> blank, !, blanks.
+blanks --> [].
+
+nl --> "\n".
+
+digit(D) --> [D], { char_type(D, decimal_digit) }.
+
+integer(I) --> digit(D), integer_(Ds), { number_chars(I, [D|Ds]) }.
+integer_([D|Ds]) --> digit(D), !, integer_(Ds).
+integer_([]) --> [].
+
+range(Start..End) --> integer(Start), "-", integer(End).
+
+line((R1,R2)) --> range(R1), ",", range(R2).
+
+parse([L|Ls]) --> blanks, line(L), !, parse_(Ls), blanks.
+parse_([L|Ls]) --> nl, line(L), !, parse_(Ls).
+parse_([]) --> [].
+
+:- op(900, xfx, contains).
+
+contains(AStart..AEnd, BStart..BEnd) :-
+  AStart =< BStart,
+  AEnd >= BEnd.
+
+:- op(900, xfx, overlaps).
+
+overlaps(AStart..AEnd, BStart..BEnd) :-
+  AStart =< BEnd,
+  BStart =< AEnd.
+
+part1(Data, Res) :-
+  findall(1,
+    ( member((A,B), Data),
+      once((A contains B; B contains A))
+    ),
+    Xs),
+  length(Xs, Res).
+part2(Data, Res) :-
+  findall(1,
+    ( member((A,B), Data),
+      A overlaps B
+    ),
+    Xs),
+  length(Xs, Res).
